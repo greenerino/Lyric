46f587f2ed9b4b3030e69c786711c05e9bfcde3d
greenerino <greenejustint@gmail.com>
2020-07-31 21:58:40 -0700

    Add animations to movements

--- scenes/Player/Player.gd
+++ scenes/Player/Player.gd
@@ -5,7 +5,9 @@ const ACCELERATION = 300
 const MAX_SPEED = 100
 const FRICTION = 300
 
-onready var animationPlayer = get_node("AnimationPlayer")
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	var input = Vector2.ZERO
@@ -13,7 +15,11 @@ func _physics_process(delta):
 	input.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 		
 	if input:
+		animationTree.set("parameters/Idle/blend_position", input)
+		animationTree.set("parameters/Run/blend_position", input)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(input.normalized() * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	velocity = move_and_slide(velocity)
