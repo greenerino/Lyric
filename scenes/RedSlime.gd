ac85a7dceb38fea461919d9ed6d3459ab6f58b01
greenerino <greenejustint@gmail.com>
2020-08-07 15:46:11 -0700

    Add chase state to red slime. Change some speed values of the slime. Add more slimes to world.

--- scenes/RedSlime.gd
+++ scenes/RedSlime.gd
@@ -5,19 +5,22 @@ onready var stats = $Stats
 const EnemyDeathEffect = preload("res://scenes/EnemyDeathEffect.tscn")
 
 const SPEED = 5
-const KNOCKBACK_SPEED = 50
-const KNOCKBACK_FRICTION = 1
+const KNOCKBACK_SPEED = 80
+const KNOCKBACK_FRICTION = 4
+const CHASE_SPEED = 20
 const MOVE_TIME = 2.0
 const WAIT_TIME = 1.0
-const STUN_TIME = 1.5
+const STUN_TIME = 0.8
 enum {
 	MOVE,
 	WAIT,
-	KNOCKED
+	KNOCKED,
+	CHASE
 }
 var state = MOVE
 var time_left = WAIT_TIME
 var velocity = Vector2.ZERO
+var playerBody = null
 
 func _ready():
 	randomize()
@@ -45,9 +48,19 @@ func _physics_process(delta):
 			velocity = velocity.move_toward(Vector2.ZERO, KNOCKBACK_FRICTION)
 			velocity = move_and_slide(velocity)
 			if time_left <= 0:
-				randomize_direction()
-				time_left = MOVE_TIME
-				state = MOVE
+				if playerBody == null:
+					randomize_direction()
+					time_left = MOVE_TIME
+					state = MOVE
+				else:
+					state = CHASE
+		CHASE:
+			if playerBody == null:
+				state = WAIT
+				time_left = WAIT_TIME
+			else:
+				velocity = velocity.move_toward(position.direction_to(playerBody.position) * CHASE_SPEED, 100)
+				velocity = move_and_slide(velocity)
 
 			
 func _on_Hurtbox_area_entered(area):
@@ -62,3 +75,11 @@ func _on_Stats_no_health():
 	var deathEffect = EnemyDeathEffect.instance()
 	get_parent().add_child(deathEffect)
 	deathEffect.position = position
+
+func _on_PlayerDetectionZone_body_entered(body):
+	playerBody = body
+	state = CHASE
+
+func _on_PlayerDetectionZone_body_exited(_body):
+	state = WAIT
+	time_left = WAIT_TIME
