36bb9425165430bf05d7d51f36193e092ab1dbf3
greenerino <greenejustint@gmail.com>
2020-08-06 18:27:07 -0700

    Add simple error check to signal connection

--- scenes/DeathEffect.gd
+++ scenes/DeathEffect.gd
@@ -1,8 +1,12 @@
 extends AnimatedSprite
 
 func _ready():
-	connect("animation_finished", self, "_on_animation_finished")
-	play()
+	var err = connect("animation_finished", self, "_on_animation_finished")
+	if err:
+		print("Error connecting signal: ", err)
+		queue_free()
+	else:
+		play()
 
 func _on_animation_finished():
 	queue_free()
